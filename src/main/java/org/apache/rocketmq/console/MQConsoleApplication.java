2d308d74b43ea92ad8032a0623b3dc8e0665506e
dayi_wei <dayiw@moguyun.com>
2019-12-03 11:21:50 +0800

    Initialize

---
+++ src/main/java/org/apache/rocketmq/console/MQConsoleApplication.java
@@ -0,0 +1,13 @@
+package org.apache.rocketmq.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MQConsoleApplication.class, args);
+    }
+
+}
